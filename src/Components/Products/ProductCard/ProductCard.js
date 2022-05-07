6d153cd72c3f9d4c3ba2773684813aaaee52f745
ShawonsGit <s.shawon0000@gmail.com>
2022-05-08 00:24:06 +0600

    product-card-fixed

--- src/Components/Products/ProductCard/ProductCard.js
+++ src/Components/Products/ProductCard/ProductCard.js
@@ -2,14 +2,15 @@ import React from 'react';
 import { Card } from 'react-bootstrap';
 import './ProductCard.css'
 const ProductCard = (props) => {
-    const {name,price,img,description} = props.product;
+    const {name,price,img,description,quantity,supplier} = props.product;
     return (
         <div>
 
-        <Card className='card-box' style={{ width: '18rem' }}>
+        <Card className='card-box' >
         <Card.Img variant="top" className='image-config' src={img} />
         <Card.Body>
             <Card.Title>{name}</Card.Title>
+            <h5>{price + '$'}</h5>
             <Card.Text>
             {description}
             </Card.Text>
