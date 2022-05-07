13c242de86b8ea46a4ae64290e57309491453621
ShawonsGit <s.shawon0000@gmail.com>
2022-05-07 22:48:21 +0600

    top-brand-section-added

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Products from '../Products/Products';
 import Banner from './Banner/Banner';
+import Brands from './Brands/Brands';
 
 const Home = () => {
     return (
@@ -9,6 +10,11 @@ const Home = () => {
             <Banner></Banner>
             </div>
             <Products></Products>
+
+            <div>
+                <h1 className='mt-5 mb-5'>Top brands</h1>
+                <Brands></Brands>
+            </div>
         </div>
     );
 };
