91ebe0116e744daafb7e0d4ea388092e79b4b052
ShawonsGit <s.shawon0000@gmail.com>
2022-05-08 20:30:07 +0600

    single detail data load done

--- src/Components/Products/ProductDetail/ProductDetail.js
+++ src/Components/Products/ProductDetail/ProductDetail.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 const ProductDetail = () => {
     const {productId} = useParams();
+    const [product,setProduct] = useState([]);
+    useEffect(()=>{
+        const url = `http://localhost:5000/products/${productId}`
+        fetch(url)
+        .then(res => res.json())
+        .then(data => setProduct(data))
+    },[])
     return (
         <div>
             <h2>Product detail</h2>
             <h2>id : {productId}</h2>
+            <h3>Name : {product.name}</h3>
         </div>
     );
 };
